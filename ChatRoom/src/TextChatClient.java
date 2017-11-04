fb4ee6b46c641e36f55766d72b7d94514a68f9d6
danielwunderink <dnlwndrn@dordt.edu>
2017-11-04 12:47:31 -0500

    Added name recognition.

--- ChatRoom/src/TextChatClient.java
+++ ChatRoom/src/TextChatClient.java
@@ -22,12 +22,21 @@ public class TextChatClient implements ChatClient{
 		    String userInput = "";
 	        String exitToken = "exit";
 	        ServerTextPrinter printInputFromServer = new ServerTextPrinter(clientInputFromServer, exitToken);
+	        //initial protocol
 	        printInputFromServer.start();
+	        clientOutputToServer.println(name);
+	        
+	        //main loop
 	        do {
+	        	if (userInput.startsWith("/")) {
+					String command = userInput.substring(1, userInput.length());
+					if (command.equalsIgnoreCase("exit")) {
+						break;
+					}
+				}
 	        	userInput = userInputToClient.nextLine();
-	        	System.out.println("Typed: "+userInput);
 	        	clientOutputToServer.println(userInput);
-	        } while (!userInput.equals(exitToken));
+	        } while (userInput != null);
 	        
 	        socket.close();
 	        userInputToClient.close();
@@ -64,8 +73,8 @@ public class TextChatClient implements ChatClient{
 		public void run() {
 			String input;
 			try {
-				while (!(input = inputFromServer.readLine()).equalsIgnoreCase(exitToken)) {
-		        	System.out.println("Server: "+ input);
+				while ((input = inputFromServer.readLine()) != null) {
+		        	System.out.println(input);
 		        }
 			} catch (IOException e) {
 				e.printStackTrace();
